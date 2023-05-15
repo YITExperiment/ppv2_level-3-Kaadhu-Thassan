00d4b70288ff25225d54681a90b8a4c950d0408a
Kaadhu-Thassan <130353183+Kaadhu-Thassan@users.noreply.github.com>
2023-05-15 11:00:13 +0530

    Add files via upload

--- robot_builder.py
+++ robot_builder.py
@@ -1,8 +1,8 @@
-import turtle as t 
+import turtle as t
 
 def rectangle(horizontal,vertical,color):
     t.pendown()
-    t.pensize(1)
+    t.pensize()
     t.color(color)
     t.begin_fill()
     for counter in range(1,3):
@@ -20,7 +20,7 @@ t.bgcolor('Dodger blue')
 #feet
 t.goto(-100, -150)
 rectangle(50,20,'blue')
-t.goto(-30,-150)
+t.goto(-30, -150)
 rectangle(50,20,'blue')
 
 #legs
@@ -34,12 +34,11 @@ t.goto(-90,100)
 rectangle(100,150,'red')
 
 #arms
-t.goto(-150, 70)
+t.goto(-150,70)
 rectangle(60,15,'grey')
 t.goto(-150,110)
 rectangle(15,40,'grey')
-
-t.goto(10, 70)
+t.goto(10,70)
 rectangle(60,15,'grey')
 t.goto(55,110)
 rectangle(15,40,'grey')
@@ -53,16 +52,15 @@ t.goto(-85,170)
 rectangle(80,50,'red')
 
 #eyes
-t.goto(-60, 160)
-rectangle(30,10,'white')
 t.goto(-60,160)
+rectangle(30,10,'white')
+t.goto(-55,155)
 rectangle(5,5,'black')
-t.goto(-45,155)
+t.goto(-40,155)
 rectangle(5,5,'black')
 
 #mouth
 t.goto(-65,135)
-t.right(5)
 rectangle(40,5,'black')
 
 t.hideturtle()
